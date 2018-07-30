7cac3f78975eb21afc06dc5e70bcc23b0573f5d9
Taynan Souza <taynan.tws@gmail.com>
2018-07-30 20:21:37 -0300

    42. Basic Input Biding

---
+++ Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+	void ProcessInput()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			print("space pressed");
+		}
+		// its separeted because it can rotate while thrust
+		// but cant rotate to left and right at same time
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+		{
+			print("rotating left");
+		}
+		else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+		{
+			print("rotating right");
+		}
+	}
+}
